044cbd0010fffe9506fc14f3e506995affe8e42e
eixcosxsinxi <wesleyboud@gmail.com>
2025-04-10 13:17:23 -0400

    added the dialog box with the style class

--- app/src/main/java/app/GUIApplication.java
+++ app/src/main/java/app/GUIApplication.java
@@ -1,6 +1,6 @@
 package app;
 
-import app.view.TitleWindow;
+import app.view.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
--- app/src/main/java/app/View/TitleWindow.java
+++ app/src/main/java/app/View/TitleWindow.java
@@ -24,6 +24,11 @@ public class TitleWindow {
     }
 
     @FXML
+    void onHelpClicked(ActionEvent event) {
+        displayAlert("The game Quantum Chess starts with the classical game of chess.");
+    }
+
+    @FXML
     void onStartClicked(ActionEvent event) throws Exception {
         // on start clicked, load the main window
         var mainResource = getClass().getResource("/app/MainWindow.fxml");
@@ -40,5 +45,10 @@ public class TitleWindow {
         boolean cheat = cheatMode.isSelected();
         int difficulty = (int)difficultySlider.getValue();
         mainWindow.initialize(titleStage, mainStage, cheat, difficulty);
+    }
+
+    private void displayAlert(String text) {
+        var alert = new Alert(Alert.AlertType.INFORMATION, text);
+        alert.getDialogPane().getStyleClass().add("helpScreen");
     }
 }
